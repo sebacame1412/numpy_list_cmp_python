2279bb1609e4880c718a85430a66f5fdf55f4d35
sebacame1412 <105613853+sebacame1412@users.noreply.github.com>
2022-07-18 21:43:29 -0300

    Ejercicio N°1 Finalizado

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -10,6 +10,9 @@
 # Ejercicios con lambda
 
 
+from re import X
+
+
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
     
@@ -18,8 +21,9 @@ if __name__ == '__main__':
     # Realizar una funcion lambda que eleve al cuadrado
     # el número pasado como parámetro
 
-    # potencia_2 = lambda x:......
-    # pot_3 = potencia_2(3)
+    potencia_2 = lambda x: x * x
+    pot_3 = potencia_2(3)
+    print(pot_3)
 
     # 2)
     # Utilice la función map para mapear una lambda expression
@@ -35,6 +39,8 @@ if __name__ == '__main__':
     # Lista de numeros
     numeros = [1, -5, 4, 3]
 
-    # numeros_potencia = list(map....)
+    numeros_potencia = list(map(lambda x: x*x, numeros))
+
+    print(numeros_potencia)
 
     print("terminamos")
